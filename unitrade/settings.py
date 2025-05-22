95d5b8d84be645b9a73215dfa9eabea0d4c7fd67
John Matthew Candoy <mjmcandoy@tip.edu.ph>
2025-05-22 14:25:54 +0800

    Update .env: add Render URL to ALLOWED_HOSTS

--- unitrade/settings.py
+++ unitrade/settings.py
@@ -12,8 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['unitradeproject.onrender.com', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Session and login settings
 CART_SESSION_ID = 'cart'
